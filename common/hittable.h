61e0feeee74f0ec55cf4edf570fa086f35400cac
sungpaks <emforhs03150@gmail.com>
2026-09-04 13:14:38 +0900

    feat: 6.4 hit지점이 앞면인지 뒷면인지 판단하고 저장하기

--- common/hittable.h
+++ common/hittable.h
@@ -8,6 +8,15 @@ public:
   point3 p;
   vec3 normal;
   double t;
+  bool front_face;
+
+  void set_face_normal(const ray& r, const vec3& outward_normal) {
+    // hit record의 법선벡터를 설정.
+    // "outward_normal"은 항상 단위 벡터라고 가정한다.
+
+    front_face = dot(r.direction(), outward_normal) < 0;
+    normal = front_face ? outward_normal : -outward_normal;
+  }
 };
 
 class hittable {
