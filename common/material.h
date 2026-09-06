fd3ff2a379eca739d197efe268b006e19391c92e
sungpaks <emforhs03150@gmail.com>
2026-09-06 14:44:06 +0900

    feat: 11.4 Schlick 근사 + 11.5 빈 유리공

--- common/material.h
+++ common/material.h
@@ -67,7 +67,7 @@ public:
 
     bool cannot_refract = ri * sin_theta > 1.0;
     vec3 direction;
-    if (cannot_refract)
+    if (cannot_refract || reflectance(cos_theta, ri) > random_double())
       direction = reflect(unit_direction, rec.normal);
     else
       direction = refract(unit_direction, rec.normal, ri);
@@ -79,6 +79,13 @@ public:
 private:
   double refraction_index;
   // Refraction Index 굴절률은 공기/진공 등 "바깥공간"에 대해 이 물질의 상대적인 값
+
+  static double reflectance(double cosine, double refraction_index) {
+    // Schlick's approximation을 통해 반사율을 구한다
+    auto r0 = (1 - refraction_index) / (1 + refraction_index);
+    r0 = r0 * r0;
+    return r0 + (1 - r0) * std::pow((1 - cosine), 5);
+  }
 };
 
 #endif
